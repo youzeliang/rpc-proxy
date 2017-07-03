2ba72f4602cf089b4c2cf083b0ae84c276daaab3
lyzgir <youzel@126.com>
2017-07-03 18:05:18 +0800

    test

--- learn/lprint.py
+++ learn/lprint.py
@@ -2,3 +2,16 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2017/7/2 10:26
 print('fd')
+
+a=['sd','fds','fsd']
+print(a)
+
+a[0]='aaa'
+print(a)
+
+#append  it can
+b=[]
+b.append('test')
+print(b)
+
+print("555")
