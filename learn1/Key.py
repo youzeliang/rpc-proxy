329a89f84849a7bcfe1c5e96c55238fe894e0310
lyzgir <youzel@126.com>
2017-07-22 23:29:27 +0800

    test66

---
+++ learn1/Key.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+tin={'name':'Tom','age':12}
+print(tin)
+print(tin['name'])
+
+a = 19
+n = 32
+if(a is n):
+    print("ds")
+else:
+    print("fff")
--- learn1/learn2.py
+++ learn1/learn2.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 student = {'Tom', 'Jim', 'Mary', 'Tom', 'Jack', 'Rose'}
+print(student)
+if('Tom' in student):
+    print("exist")
+else:
+    print("no")
