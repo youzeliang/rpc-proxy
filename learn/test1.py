107c1af854a0df21deab3d442332cad32ff0abad
lyzgir <youzel@126.com>
2017-07-31 18:47:10 +0800

    test66

---
+++ learn/test1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/7/31 18:02
+
+# import random
+# for i in range(5):
+#     print(random.randint(1, 5))
+
+# import sys
+# while True:
+#     print("fde")
+#     a = input()
+#     if a =="exit":
+#         sys.exit()
+#     print("fdf")
+
+def spam(f):
+    try:
+        return  1/f
+    except ZeroDivisionError:
+        print('除数不能为0')
+
+print(spam(0))
