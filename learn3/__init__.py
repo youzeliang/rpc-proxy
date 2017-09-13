cac8e51c4f330b6979b3e78a7c9928d26e8c1772
youze <youzel@126.com>
2017-09-14 07:47:23 +0800

    /

---
+++ learn3/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/8 15:06
+
+span = {'name':'Bob','age':23}
+if 'color' not in span:
+    span['color'] = 'blue'
+print(span)
---
+++ learn3/test1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/8 15:10
+theBoard = {
+    'top-L':"", 'top':'','top-r':'',
+    'top-L':"", 'top':'','top-r':'',
+    'top-L':"", 'top':'','top-r':'',
+}
+print(theBoard)
+print('ffdffd')
