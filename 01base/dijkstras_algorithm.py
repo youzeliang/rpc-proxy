017088b39f2bdd18d9faee14253b7e26d97c0f81
yzrds <youzel@126.com>
2018-06-11 17:04:11 +0800

    breadth-first_search

--- 01base/dijkstras_algorithm.py
+++ 01base/dijkstras_algorithm.py
@@ -28,6 +28,7 @@ parents["fin"] = None
 
 processed = []
 
+
 def find_lowest_cost_node(costs):
     lowest_cost = float("inf")
     lowest_cost_node = None
@@ -40,6 +41,7 @@ def find_lowest_cost_node(costs):
             lowest_cost = cost
             lowest_cost_node = node
     return lowest_cost_node
+
 
 # Find the lowest-cost node that you haven't processed yet.
 node = find_lowest_cost_node(costs)
@@ -61,5 +63,5 @@ while node is not None:
     # Find the next node to process, and loop.
     node = find_lowest_cost_node(costs)
 
-print ("Cost from the start to each node:")
-print (costs)
+print("Cost from the start to each node:")
+print(costs)
---
+++ learn1/test.py
@@ -0,0 +1,6 @@
+x = pow(2,572352)
+
+print(x)
+print(len(str(x)))
+
+print(111)
